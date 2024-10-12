4bcd3dd679732d09908c6643ca1125f267cd978b
Jeane Feliciano <jrvnflcn@gmail.com>
2024-10-12 09:25:54 +0800

    adjusted project 5

--- Project 5/loading.js
+++ Project 5/loading.js
@@ -1,17 +1,15 @@
 function fadeOutContainer() {
     const container = document.querySelector('.loadingio-spinner-ellipsis-nq4q5u6dq7r');
     const inputPage = document.getElementById('inputPage');
-    
-    container.classList.add('fade-out'); // Add fade-out class
 
-    // Wait for fade-out to complete before displaying the input page
+    container.classList.add('fade-out'); 
+
     setTimeout(() => {
-        container.style.display = 'none'; // Hide loading container // Show input page
+        container.style.display = 'none'; 
         inputPage.classList.remove('d-none');
-        inputPage.classList.add('fade-in'); // Add fade-in class
-    }, 1000); // Duration of the fade-out animation
+        inputPage.classList.add('fade-in'); 
+    }, 1000); 
 }
 
-// Wait until the last image's animation is complete
-const lastAnimationDelay = 6; // Delay of the last image
-setTimeout(fadeOutContainer, (lastAnimationDelay + 1) * 1000); // Add 1 second buffer
+const lastAnimationDelay = 6; 
+setTimeout(fadeOutContainer, (lastAnimationDelay + 1) * 1000); 
